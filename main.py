59f9909adef2f3951a86d30522b9cf80477281a4
Damir-Sayfullin <saifullin.d02@mail.ru>
2024-04-05 16:38:17 +0300

    Added settings profile system

--- main.py
+++ main.py
@@ -4,20 +4,84 @@ import operations
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 
+class Profile:
+    def __init__(self, name, ip, mac_address, use_ip, username, password):
+        self.name = name
+        self.ip = ip
+        self.mac_address = mac_address
+        self.use_ip = use_ip
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return self.name
+
+
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Управление компьютером')
-        self.setFixedSize(400, 230)
+        self.setWindowTitle('ServerControl')
+        self.setFixedSize(420, 320)
         self.setWindowIcon(QtGui.QIcon('icons/icon.ico'))
-        self.ip = '192.168.0.106'
-        self.mac_address = '00:30:67:25:79:43'
-        self.use_ip = False
-        self.username = 'damir'
-        self.password = '123'
+        self.selected_profile = Profile('Default', '0.0.0.0', '00:00:00:00:00:00',
+                                        True, 'admin', 'admin')
         self.profile_file = 'profiles.json'
+        self.profiles = self.load_profiles()
         self.init_ui()
+        self.check_profiles()
         self.show()
+
+    def load_profiles(self):
+        try:
+            with open(self.profile_file, 'r') as f:
+                data = f.read()
+                if data.strip():
+                    f.seek(0)
+                    json_data = json.load(f)
+                    validated_profiles = []
+                    for profile_name, profile_data in json_data.items():
+                        if self.validate_profile(profile_data) and profile_name.strip():
+                            validated_profiles.append(Profile(profile_name, profile_data['ip'].strip(),
+                                                              profile_data['mac_address'].strip(),
+                                                              profile_data['use_ip'],
+                                                              profile_data['username'].strip(),
+                                                              profile_data['password'].strip()))
+                        else:
+                            return -1
+                    return validated_profiles
+                else:
+                    return [self.selected_profile]
+        except FileNotFoundError:
+            return [self.selected_profile]
+        except json.JSONDecodeError:
+            return -1
+
+    def validate_profile(self, profile_data):
+        if (isinstance(profile_data, dict) and
+                all(key in profile_data for key in ['ip', 'mac_address', 'use_ip', 'username', 'password'])):
+            # Проверка на пустоту значений
+            if (all(profile_data[key].strip() for key in ['ip', 'mac_address', 'username', 'password']) and
+                    isinstance(profile_data['use_ip'], bool)):
+                ip_pattern = r'^(\d{1,3}\.){3}\d{1,3}$'
+                mac_pattern = r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+                if re.match(ip_pattern, profile_data['ip']) and re.match(mac_pattern, profile_data['mac_address']):
+                    return True
+        else:
+            return False
+
+    def check_profiles(self):
+        if self.profiles != -1:
+            self.update_profiles()
+        else:
+            self.label_profile.setText('Профили недоступны:\n'
+                                       'Укажите другой файл в настройках.')
+            self.label_profile.setEnabled(False)
+            self.list_profiles.setEnabled(False)
+            self.button_save_profiles.setEnabled(False)
+
+    def update_profiles(self):
+        for profile in self.profiles:
+            self.list_profiles.addItem(profile.name)
 
     def init_ui(self):
         self.main_vbox = QtWidgets.QVBoxLayout()
@@ -28,20 +92,37 @@ class MainWindow(QtWidgets.QWidget):
         self.label_status = QtWidgets.QLabel('Статус: Неизвестно.', self)
         self.main_vbox.addWidget(self.label_status)
 
-        self.label_ip = QtWidgets.QLabel(f'IP адрес компьютера: \n{self.ip}', self)
+        self.label_ip = QtWidgets.QLabel(f'IP адрес компьютера: \n{self.selected_profile.ip}', self)
         self.vbox1.addWidget(self.label_ip)
 
-        self.label_mac_address = QtWidgets.QLabel(f'MAC адрес компьютера: \n{self.mac_address}', self)
+        self.label_mac_address = QtWidgets.QLabel(f'MAC адрес компьютера: \n{self.selected_profile.mac_address}', self)
         self.vbox1.addWidget(self.label_mac_address)
 
-        self.label_use_ip = QtWidgets.QLabel(f'Использовать IP адрес: \n{"Да" if self.use_ip else "Нет"}', self)
+        self.label_use_ip = QtWidgets.QLabel(
+            f'Использовать IP адрес: \n{"Да" if self.selected_profile.use_ip else "Нет"}', self)
         self.vbox1.addWidget(self.label_use_ip)
 
-        self.label_username = QtWidgets.QLabel(f'Имя пользователя: \n{self.username}', self)
+        self.label_username = QtWidgets.QLabel(f'Имя пользователя: \n{self.selected_profile.username}', self)
         self.vbox1.addWidget(self.label_username)
 
-        self.label_password = QtWidgets.QLabel(f'Пароль пользователя: \n{self.password}', self)
+        self.label_password = QtWidgets.QLabel(f'Пароль пользователя: \n{self.selected_profile.password}', self)
         self.vbox1.addWidget(self.label_password)
+
+        self.label_profile = QtWidgets.QLabel(f'Профиль:', self)
+        self.label_profile.setFixedSize(350, 35)
+        self.vbox2.addWidget(self.label_profile)
+
+        self.vbox2.addSpacing(10)
+
+        self.list_profiles = QtWidgets.QComboBox(self)
+        self.list_profiles.currentTextChanged.connect(self.list_profiles_changed)
+        self.vbox2.addWidget(self.list_profiles)
+
+        self.button_save_profiles = QtWidgets.QPushButton(QtGui.QIcon('icons/save.ico'), 'Сохранить профили', self)
+        self.button_save_profiles.clicked.connect(self.button_save_profiles_clicked)
+        self.vbox2.addWidget(self.button_save_profiles)
+
+        self.vbox2.addSpacing(self.height())
 
         self.label_select = QtWidgets.QLabel('Выберите действие:')
         self.vbox2.addWidget(self.label_select)
@@ -63,6 +144,7 @@ class MainWindow(QtWidgets.QWidget):
         self.vbox2.addWidget(self.button_settings)
 
         self.hbox.addLayout(self.vbox1)
+        self.hbox.addSpacing(20)
         self.hbox.addLayout(self.vbox2)
         self.main_vbox.addLayout(self.hbox)
         self.setLayout(self.main_vbox)
@@ -70,60 +152,63 @@ class MainWindow(QtWidgets.QWidget):
     def button_wake_clicked(self):
         self.label_status.setText('Статус: Включение компьютера...')
         QtCore.QCoreApplication.processEvents()
-        result = operations.wake_on_lan(self.mac_address, self.ip) if self.use_ip else operations.wake_on_lan(
-            self.mac_address)
+        result = operations.wake_on_lan(self.selected_profile.mac_address, self.selected_profile.ip) \
+            if self.selected_profile.use_ip else operations.wake_on_lan(self.selected_profile.mac_address)
         if result:
             self.label_status.setText('Ошибка: ' + result)
         else:
             self.label_status.setText('Статус: Компьютер включен.')
 
     def button_ssh_clicked(self):
-        result = operations.connect_by_ssh(self.ip, self.username)
+        result = operations.connect_by_ssh(self.selected_profile.ip, self.selected_profile.username)
         if result:
             self.label_status.setText('Ошибка: ' + result)
         else:
             self.label_status.setText('Статус: Подключение по ssh выполнено.')
 
     def button_shutdown_clicked(self):
-        result = operations.shutdown(self.ip, self.username, self.password)
+        result = operations.shutdown(self.selected_profile.ip, self.selected_profile.username,
+                                     self.selected_profile.password)
         if result:
             self.label_status.setText('Ошибка: ' + result)
         else:
             self.label_status.setText('Статус: Компьютер выключен.')
 
-    def button_settings_clicked(self):
-        self.settings = SettingsWindow(self.ip, self.mac_address, self.use_ip, self.username,
-                                       self.password, self.settings_changed)
+    def button_save_profiles_clicked(self):
+        with open('profiles.json', 'w') as f:
+            data = {}
+            for profile in self.profiles:
+                data[profile.name] = {
+                    'ip': profile.ip,
+                    'mac_address': profile.mac_address,
+                    'use_ip': profile.use_ip,
+                    'username': profile.username,
+                    'password': profile.password
+                }
+            json.dump(data, f, indent=4)
 
-    def settings_changed(self, setting_name, new_value):
-        if setting_name == 'ip':
-            self.ip = new_value
-            self.label_ip.setText(f'IP адрес компьютера: \n{self.ip}')
-        elif setting_name =='mac_address':
-            self.mac_address = new_value
-            self.label_mac_address.setText(f'MAC адрес компьютера: \n{self.mac_address}')
-        elif setting_name == 'use_ip':
-            self.use_ip = new_value
-            self.label_use_ip.setText(f'Использовать IP адрес: \n{"Да" if self.use_ip else "Нет"}')
-        elif setting_name == 'username':
-            self.username = new_value
-            self.label_username.setText(f'Имя пользователя: \n{self.username}')
-        elif setting_name == 'password':
-            self.password = new_value
-            self.label_password.setText(f'Пароль пользователя: \n{self.password}')
+    def settings_changed(self):
+        self.label_ip.setText(f'IP адрес компьютера: \n{self.selected_profile.ip}')
+        self.label_mac_address.setText(f'MAC адрес компьютера: \n{self.selected_profile.mac_address}')
+        self.label_use_ip.setText(f'Использовать IP адрес: \n{"Да" if self.selected_profile.use_ip else "Нет"}')
+        self.label_username.setText(f'Имя пользователя: \n{self.selected_profile.username}')
+        self.label_password.setText(f'Пароль пользователя: \n{self.selected_profile.password}')
+
+    def list_profiles_changed(self):
+        self.selected_profile = self.profiles[self.list_profiles.currentIndex()]
+        self.settings_changed()
+
+    def button_settings_clicked(self):
+        self.settings = SettingsWindow(self.selected_profile, self.settings_changed)
 
 
 class SettingsWindow(QtWidgets.QWidget):
-    def __init__(self, ip, mac_address, use_ip, username, password, settings_changed):
+    def __init__(self, selected_profile, settings_changed):
         super().__init__()
         self.setWindowTitle('Настройки')
         self.setFixedSize(300, 300)
         self.setWindowIcon(QtGui.QIcon('icons/settings.ico'))
-        self.ip = ip
-        self.mac_address = mac_address
-        self.use_ip = use_ip
-        self.username = username
-        self.password = password
+        self.selected_profile = selected_profile
         self.settings_changed = settings_changed
         self.init_ui()
         self.show()
@@ -136,14 +221,14 @@ class SettingsWindow(QtWidgets.QWidget):
 
         self.lineEdit_ip = QtWidgets.QLineEdit()
         self.lineEdit_ip.setInputMask('000.000.000.000')
-        self.lineEdit_ip.setText(self.ip)
+        self.lineEdit_ip.setText(self.selected_profile.ip)
         self.vbox.addWidget(self.lineEdit_ip)
 
         self.label_mac_address = QtWidgets.QLabel('MAC-адрес компьютера:')
         self.vbox.addWidget(self.label_mac_address)
 
         self.lineEdit_mac_address = QtWidgets.QLineEdit()
-        self.lineEdit_mac_address.setText(self.mac_address)
+        self.lineEdit_mac_address.setText(self.selected_profile.mac_address)
         self.lineEdit_mac_address.setInputMask('HH:HH:HH:HH:HH:HH')
         self.vbox.addWidget(self.lineEdit_mac_address)
 
@@ -151,18 +236,18 @@ class SettingsWindow(QtWidgets.QWidget):
         self.vbox.addWidget(self.label_username)
 
         self.lineEdit_username = QtWidgets.QLineEdit()
-        self.lineEdit_username.setText(self.username)
+        self.lineEdit_username.setText(self.selected_profile.username)
         self.vbox.addWidget(self.lineEdit_username)
 
         self.label_password = QtWidgets.QLabel('Пароль пользователя:')
         self.vbox.addWidget(self.label_password)
 
         self.lineEdit_password = QtWidgets.QLineEdit()
-        self.lineEdit_password.setText(self.password)
+        self.lineEdit_password.setText(self.selected_profile.password)
         self.vbox.addWidget(self.lineEdit_password)
 
         self.checkBox_use_ip = QtWidgets.QCheckBox('[Wake-on-LAN] Использовать IP-адрес')
-        self.checkBox_use_ip.setChecked(self.use_ip)
+        self.checkBox_use_ip.setChecked(self.selected_profile.use_ip)
         self.vbox.addWidget(self.checkBox_use_ip)
 
         self.button_save = QtWidgets.QPushButton(QtGui.QIcon('icons/save.ico'), 'Сохранить и закрыть', self)
@@ -191,11 +276,12 @@ class SettingsWindow(QtWidgets.QWidget):
             QtWidgets.QMessageBox.warning(self, 'Предупреждение', 'Пожалуйста, заполните все поля.')
             return
 
-        self.settings_changed('ip', new_ip)
-        self.settings_changed('mac_address', new_mac_address)
-        self.settings_changed('use_ip', new_use_ip)
-        self.settings_changed('username', new_username)
-        self.settings_changed('password', new_password)
+        self.selected_profile.ip = new_ip
+        self.selected_profile.mac_address = new_mac_address
+        self.selected_profile.use_ip = new_use_ip
+        self.selected_profile.username = new_username
+        self.selected_profile.password = new_password
+        self.settings_changed()
         self.close()
 
 
